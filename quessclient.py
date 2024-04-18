29634ac901213c51f8d156e755c3467edfe1c2b1
Matthew Earl <git@matthewearl.com>
2024-04-18 19:13:44 +0100

    tentative stockfish integration
    think it needs to run in a thread since connection dies when thinking

--- quessclient.py
+++ quessclient.py
@@ -3,6 +3,7 @@ import enum
 import logging
 
 import numpy as np
+import stockfish
 
 import pyquake.client
 import pyquake.proto
@@ -51,27 +52,69 @@ def _get_board_state(client):
         model = client.models[ent.model_num - 1]
         if model in _model_to_piece:
             piece = _model_to_piece[model]
+            unrounded_coords = (np.array(ent.origin[:2]) + 224) / 64
             coords = np.round((np.array(ent.origin[:2]) + 224) / 64).astype(int)
+            if np.any(np.abs(unrounded_coords - coords) > 0.2):
+                return None
             side = Side.WHITE if ent.skin == 1 else Side.BLACK
-            board_state[tuple(coords)] = (side, piece)
+            board_state[ent.entity_num] = (tuple(coords), side, piece)
 
     return board_state
+
+
+def _board_state_to_fen(board_state):
+    chars = [['.' for _ in range(8)] for _ in range(8)]
+
+    for (x, y), side, piece in board_state.values():
+        char = _piece_to_notation[piece]
+        if side == Side.BLACK:
+            char = char.lower()
+        chars[y][x] = char
+
+    field1 = '/'.join(
+        ''.join(
+            chars[y][x]
+            for x in range(8)
+        )
+        for y in range(8)
+    )
+    for n in reversed(range(1, 9)):
+        field1 = field1.replace('.' * n, str(n))
+
+    return f'{field1} w - - 0 1'
+
+
+def _diff_board_state(bs1, bs2):
+    moves = []
+    for k in bs1.keys() & bs2.keys():
+        (coords1, side1, piece1) = bs1[k]
+        (coords2, side2, piece2) = bs2[k]
+        assert side1 == side2
+        assert piece1 == piece2
+        if coords1 != coords2:
+            moves.append((coords1, coords2))
+    return moves
 
 
 def _print_board_state(board_state):
     chars = [['.' for _ in range(8)] for _ in range(8)]
 
-    for (x, y), (side, piece) in board_state.items():
+    for (x, y), side, piece in board_state.values():
         char = _piece_to_notation[piece]
-        if side == Side.WHITE:
+        if side == Side.BLACK:
             char = char.lower()
         chars[y][x] = char
 
-    print('\n'.join(' '.join(c for c in row) for row in chars))
+    print('\n'.join(' '.join(c for c in row) for row in reversed(chars)))
 
 
 def _square_location(x, y):
     return (np.array([x, y, -16]) - [3.5, 3.5, 0]) * [64, 64, 1]
+
+
+def _decode_move(move):
+    return ((ord(move[0]) - ord('a'), ord(move[1]) - ord('1')),
+            (ord(move[2]) - ord('a'), ord(move[3]) - ord('1')))
 
 
 async def do_client():
@@ -82,28 +125,55 @@ async def do_client():
             pyquake.proto.ProtocolFlags(0)
         )
     )
+    sf = stockfish.Stockfish()
     try:
         demo = client.record_demo()
         await client.wait_until_spawn()
         start_time = client.time
 
+        last_board_state = None
+        from_coords = None
+        to_coords = None
         while True:
-            while not client.center_print_queue.empty():
-                print('center print!', client.center_print_queue.get_nowait())
-            view_origin = np.array(client.player_entity.origin)
-            view_origin[2] += client.view_height
-            target = _square_location(int(client.time) & 7,
-                                      (int(client.time) >> 3) & 0x7)
-            dir_ = target - view_origin
-            yaw = np.arctan2(dir_[1], dir_[0])
-            pitch = np.arctan2(-dir_[2], np.linalg.norm(dir_[:2]))
+            if from_coords is None:
+                yaw = 0
+                pitch = 0
+                impulse = 0
+            else:
+                view_origin = np.array(client.player_entity.origin)
+                view_origin[2] += client.view_height
+                phase = int(client.time * 10) % 6
+                if phase < 3:
+                    coords = from_coords
+                else:
+                    coords = to_coords
+                target = _square_location(*coords)
+                dir_ = target - view_origin
+                yaw = np.arctan2(dir_[1], dir_[0])
+                pitch = np.arctan2(-dir_[2], np.linalg.norm(dir_[:2]))
+                impulse = 100 if phase % 3 == 0 else 0
 
-            client.move(pitch, yaw, 0, 0, 0, 0, 0, 0)
+            client.move(pitch, yaw, 0, 0, 0, 0, 0, impulse)
 
             await client.wait_for_movement(client.view_entity)
 
-            print('-----')
-            _print_board_state(_get_board_state(client))
+            board_state = _get_board_state(client)
+            if board_state is not None:
+                if last_board_state is not None:
+                    moves = _diff_board_state(last_board_state, board_state)
+
+                if last_board_state is None or moves:
+                    print()
+                    _print_board_state(board_state)
+                    fen_state = _board_state_to_fen(board_state)
+                    print(fen_state)
+
+                    sf.set_fen_position(fen_state)
+                    encoded_move = sf.get_best_move()
+                    from_coords, to_coords = _decode_move(encoded_move)
+                    print(f'playing {encoded_move} ({from_coords}-{to_coords})')
+
+                last_board_state = board_state
 
     finally:
         await client.disconnect()
