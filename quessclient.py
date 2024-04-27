adfb531616cb972041542ec62ee93063752222f0
Matthew Earl <git@matthewearl.com>
2024-04-27 15:24:52 +0100

    log timestamps too

--- quessclient.py
+++ quessclient.py
@@ -452,5 +452,8 @@ async def do_client():
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
     asyncio.run(do_client())
